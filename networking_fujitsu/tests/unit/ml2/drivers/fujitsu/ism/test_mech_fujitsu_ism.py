de091f663592d01994868c7c394fd3cc38991730
Yushiro FURUKAWA <y.furukawa_2@jp.fujitsu.com>
2016-12-27 22:45:07 +0900

    Remove unnecessary tests
    Change-Id: I5378714e31bb188c7b38db6eb7186ac6de0ce29c
    (cherry picked from commit I5f487643c7ce280f79fb735162994e72f6a630db)

--- networking_fujitsu/tests/unit/ml2/drivers/fujitsu/ism/test_mech_fujitsu_ism.py
+++ networking_fujitsu/tests/unit/ml2/drivers/fujitsu/ism/test_mech_fujitsu_ism.py
@@ -19,10 +19,7 @@ test_networking_fujitsu
 
 Tests for `networking_fujitsu` module.
 """
-from mock import MagicMock
-
 from networking_fujitsu.ml2.drivers.fujitsu.ism import mech_fujitsu_ism
-from networking_fujitsu.tests.unit.ml2.drivers.fujitsu.ism import ism_common
 from neutron.tests.unit.plugins.ml2 import test_plugin as test_ml2_plugin
 from oslo_log import log as logging
 
@@ -46,44 +43,3 @@ class FujitsuIsmDriverTestCase(test_ml2_plugin.Ml2PluginV2TestCase):
 
     def tearDown(self):
         super(FujitsuIsmDriverTestCase, self).tearDown()
-
-    def prepare(self, net={}, port={}):
-        mock_net, mock_port = self.prepare_net_and_port(net, port)
-        network_type = mock_net['provider:network_type']
-        seg_id = mock_net['provider:segmentation_id']
-        ism_common.IsmMockFactory(network_type, seg_id)
-        return self.prepare_dummy_context(mock_net, mock_port)
-
-    def prepare_net_and_port(self, net_params={}, port_params={}):
-        neutron = ism_common.NeutronFactory()
-        mock_net = neutron.build_network(net_params)
-        mock_port = neutron.build_port(mock_net, port_params)
-        return mock_net, mock_port
-
-    def prepare_dummy_context(self, mock_net, mock_port):
-        context = ism_common.DummyContext(mock_net, mock_port)
-        self.driver.get_network = MagicMock(return_value=mock_net)
-        return context
-
-    # Test Start
-    # def test_create_port_vnic_type_is_not_baremetal(self):
-    #    port = {'binding:vnic_type': 'hogemetal'}
-    #    context = self.prepare(net={}, port=port)
-    #    result = self._driver.create_port_postcommit(context)
-    #    self.assertFalse(result)
-
-    # def test_create_port_local_link_is_empty_list(self):
-    #    port = {'local_link_information': {}}
-    #    context = self.prepare(net={}, port=port)
-    #    result = self._driver.create_port_postcommit(context)
-    #    self.assertFalse(result)
-
-    # def test_create_port_success(self):
-    #    context = self.prepare(net={}, port={})
-    #    result = self._driver.create_port_postcommit(context)
-    #    self.assertFalse(result)
-
-
-class TestFujitsuMechDriverPortsV2(test_ml2_plugin.TestMl2PortsV2,
-                                   FujitsuIsmDriverTestCase):
-    pass
