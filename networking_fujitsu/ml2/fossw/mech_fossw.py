170a9f5191186f5b76253c11726e780c665e92ad
Lujin <luo.lujin@jp.fujitsu.com>
2017-09-15 19:02:23 +0900

    Remove delete_network_postcommit()
    Since [1] gets merged, a segment of network cannnot be referred. This
    commit workarounds the problem by removing delete_network_postcommit()
    and corresponding tests.
    
    [1] https://review.openstack.org/#/c/429441/
    
    Change-Id: I330db14ca61d938349ada7dee51b2fe99a69e003
    Closes-Bug: #1695208

--- networking_fujitsu/ml2/fossw/mech_fossw.py
+++ networking_fujitsu/ml2/fossw/mech_fossw.py
@@ -169,68 +169,6 @@ class FOSSWMechanismDriver(api.MechanismDriver):
             raise ml2_exc.MechanismDriverError(method=method)
 
     @log_helpers.log_method_call
-    def delete_network_postcommit(self, net_context):
-        """Calls clean process for FOS switch.
-
-        Case1: Baremetal deploy with VLAN network
-                   Delete VLAN definition.
-        Case2: Baremetal deploy with VXLAN network
-                   Delete DCVPN definition.
-        Case3: Otherwise:
-                   Do nothing.
-
-        :param net_context: context of network
-        :type net_context: NetworkContext
-
-        :returns: None
-        :rtype: None
-        """
-
-        network = net_context.current
-        net_id = network['id']
-        tenant_id = network['tenant_id']
-        network_type = utils.get_network_type(network)
-        seg_id = utils.get_segmentation_id(network)
-
-        if network_type == nl_const.TYPE_VLAN and seg_id:
-            self.delete_network_postcommit_vlan(seg_id)
-        if network_type == nl_const.TYPE_VXLAN and seg_id:
-            self.delete_network_postcommit_vxlan(net_id)
-        LOG.info(
-            "Deleted network (postcommit): network_id=%(net)s "
-            "tenant_id=%(tenant)s", {'net': net_id, 'tenant': tenant_id})
-
-    @log_helpers.log_method_call
-    def delete_network_postcommit_vlan(self, vlanid):
-        """Calls clean vlan process for FOS switch.
-
-        :param vlanid: the ID of VLAN to be deleted
-        :type vlanid: string
-
-        :returns: None
-        :rtypes: None
-
-        """
-        method = 'delete_network_postcommit'
-        for ip in self.ips:
-            try:
-                self._vlan_driver.delete_vlan(ip, vlanid)
-            except Exception:
-                LOG.exception(
-                    "Failed to validate on switch(%(ip)s).", {'ip': ip})
-                raise ml2_exc.MechanismDriverError(method=method)
-
-    @log_helpers.log_method_call
-    def delete_network_postcommit_vxlan(self, net_uuid):
-        method = 'delete_network_postcommit'
-        try:
-            self._vxlan_driver.delete_logical_switch(net_uuid)
-        except Exception:
-            LOG.exception(
-                "Failed to validate on switch(%(ip)s).", {'ip': self.ips})
-            raise ml2_exc.MechanismDriverError(method=method)
-
-    @log_helpers.log_method_call
     def delete_port_postcommit(self, mech_context):
         """Calls cleanup process for FOS switch.
 
